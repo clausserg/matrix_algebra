314b8c07f47c0362f4aaae2324c944ef0aeb7e08
Dumitru-Claudiu Sergentu <dumitruc@buffalo.edu>
2021-01-31 15:15:54 -0500

    adding matrix determinant capability

---
+++ matrix_algebra/matrix_operations/mat_det.py
@@ -0,0 +1,33 @@
+"""
+Module:
+mat_det.py
+
+This module contains a function that calculates the determinant of a matrix
+represented as python list of lists.
+"""
+
+
+def det_tt(my_mat):
+    """
+    This function returns a float, the determinant of a 2x2 matrix,
+    the difference between the product of the main diagonal elements and
+    the product of the second diagonal elements. The matrix input here, is a python list of lists.
+
+    :return: float, determinant of a two-by-two matrix
+    """
+    return my_mat[0][0] * my_mat[1][1] - my_mat[0][1] * my_mat[1][0]
+
+
+def sub_matrix(my_mat, my_row, my_col):
+    """
+    This function returns a sub-matrix of 'my_mat' resulting from the
+    elimination of the 'my_row' and 'my_col' elements.
+
+    :param my_mat: a list of lists i.e., a matrix
+    :param my_row: int, the row of 'my_mat' to be eliminated
+    :param my_col: int, the column of 'my_mat' to be eliminated
+    :return: a python list of lists
+    """
+
+    pass
+
