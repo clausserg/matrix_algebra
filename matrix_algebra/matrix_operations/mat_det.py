3d41821f3df466f2b2c7571a26ed8021f203333d
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-02-01 20:36:54 -0500

    updated mat_det.py

--- matrix_algebra/matrix_operations/mat_det.py
+++ matrix_algebra/matrix_operations/mat_det.py
@@ -6,19 +6,21 @@ This module contains a function that calculates the determinant of a matrix
 represented as python list of lists.
 """
 
+import copy
+
 
 def det_tt(my_mat):
     """
-    This function returns a float, the determinant of a 2x2 matrix,
+    This function implements the determinant of a 2x2 matrix given by the Leibniz formula, i.e.
     the difference between the product of the main diagonal elements and
-    the product of the second diagonal elements. The matrix input here, is a python list of lists.
+    the product of the second diagonal elements.
 
     :return: float, determinant of a two-by-two matrix
     """
     return my_mat[0][0] * my_mat[1][1] - my_mat[0][1] * my_mat[1][0]
 
 
-def sub_mat(my_mat, my_row, my_col):
+def sub_mat(in_mat, my_row, my_col):
     """
     This function returns a sub-matrix of 'my_mat' resulting from the
     elimination of the 'my_row' and 'my_col' elements.
@@ -29,6 +31,7 @@ def sub_mat(my_mat, my_row, my_col):
     :return: a python list of lists
     """
 
+    my_mat = copy.deepcopy(in_mat)
     trash_row = my_mat.pop(my_row)  # remove the my_row first
     trash_col = [col.pop(my_col) for col in my_mat]  # remove the column
 
