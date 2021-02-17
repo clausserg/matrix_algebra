ed5cb147bc7dd52ed409b04f384be1de79ce3b89
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-02-17 10:36:10 -0500

    added mat_det capability to main

--- matrix_algebra/matrix_algebra.py
+++ matrix_algebra/matrix_algebra.py
@@ -1,15 +1,15 @@
 """
-This is a program dealing with matrix algebra. The functions defined herein mimic Numpy functions.
+This is a Math program written in python. The functions defined herein mimic Numpy functions.
 I decided to build this in order to practice what I've learned during Udemy courses.
 
 This program is intentionally made without OBJECT ORIENTATION.
 Please checkout my other packages for an implementation of this program using object orientation.
 """
 
-
-from matrix_operations.mat_add  import *
+from matrix_operations.mat_add import *
 from matrix_operations.mat_diff import *
-from matrix_operations.mat_mul  import *
+from matrix_operations.mat_mul import *
+from matrix_operations.mat_det import *
 from input_output.input_output import *
 
 
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     operations = {
         '1': 'matrix addition',
         '2': 'matrix subtraction',
-        '3': 'matrix multiplication'
+        '3': 'matrix multiplication',
+        '4': 'matrix determinant'
     }
 
     # flag to break out of the while loop
@@ -36,6 +37,8 @@ if __name__ == '__main__':
             mat_out(a_mat)
             print("Matrix number 2 is:")
             mat_out(b_mat)
+        elif to_do in ['4']:
+            a_mat = get_mat(1)
 
         # let's do the requested operation
         if to_do == '1':
@@ -49,5 +52,9 @@ if __name__ == '__main__':
         elif to_do == '3':
             c_mat = mat_mul(a_mat, b_mat)
             mat_out(c_mat)
+            operating = False
+        elif to_do == '4':
+            det = mat_det(a_mat)
+            print(det)
             operating = False
 
--- matrix_algebra/matrix_operations/mat_det.py
+++ matrix_algebra/matrix_operations/mat_det.py
@@ -67,6 +67,14 @@ def mat_det(in_mat):
     :param in_mat: list of lists, i.e. a matrix
     :return: float or int, matrix determinant
     """
+    
+    # check if operation can be done
+    if len(in_mat) == len(in_mat[0]):  # this is not a square matrix
+        print("The matrix determinant is:")
+        pass
+    else:
+        return "You cannot calculate the determinant of this matrix!\n" \
+               "The matrix must be a square matrix!\n"
 
     # define some miscellaneous
     factors = {}  # dictionary with iteration count as key and list of factors as values
@@ -93,3 +101,4 @@ def mat_det(in_mat):
             # carry out the product with next layer
             my_det = [x[0] * x[1] for x in zip(my_det, factors[str(layer)])]
     return sum(my_det)
+
