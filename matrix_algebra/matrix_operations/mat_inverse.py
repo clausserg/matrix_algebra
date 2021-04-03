c20a9b56b19e28d56f5578f71ffb4688d9546fc9
Dumitru-Claudiu <claudiu.sergentu@gmail.com>
2021-04-03 16:25:19 -0400

    starting a module to calculate a matrix inverse

---
+++ matrix_algebra/matrix_operations/mat_inverse.py
@@ -0,0 +1,18 @@
+"""
+Module:
+mat_inverse.py
+
+This module finds the inverse of a matrix by the Gauss-Jordan method
+"""
+
+
+def mat_inv(in_mat):
+    """
+    This function returns the a matrix invers through elementary row operations
+
+    :param in_mat: list of lists i.e., a matrix
+    :return: list of lists, i.e. a matrix
+    """
+
+    # implement me
+    pass
