af1ec77a6290bab047470d2dcc9288398f0abff3
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-04-03 20:25:28 -0400

    function to swap rows in a matrix

--- matrix_algebra/matrix_operations/mat_inverse.py
+++ matrix_algebra/matrix_operations/mat_inverse.py
@@ -6,6 +6,20 @@ This module finds the inverse of a matrix by the Gauss-Jordan method
 """
 
 
+def swap_rows(mat_in, row_a,  row_b):
+    """
+    This function swaps a matrix row with another row.
+
+    :param mat_in: list of lists, i.e. a matrix
+    :param row: int
+    :param col: int
+    :return: list of lists, i.e. a matrix
+    """
+
+    mat_in[row_a], mat_in[row_b] = mat_in[row_b], mat_in[row_a]
+    return mat_in
+
+
 def mat_inv(in_mat):
     """
     This function returns the a matrix invers through elementary row operations
