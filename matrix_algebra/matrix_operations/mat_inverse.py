040de36e997017983662ece6e27d5e21badc6157
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-04-05 20:00:56 -0400

    added a function to create pivots

--- matrix_algebra/matrix_operations/mat_inverse.py
+++ matrix_algebra/matrix_operations/mat_inverse.py
@@ -22,6 +22,20 @@ def swap_rows(mat_in, row_a,  row_b):
     return mat_in
 
 
+def pivot(mat_in, row_idx,  col_idx):
+    """
+    This function creates pivot in a given row/col position of the input matrix
+
+    :param mat_in: list of lists, i.e. a matrix
+    :param row_idx: int
+    :param col_idx: int
+    :return: list of lists, i.e. a matrix
+    """
+
+    mat_in[row_idx] = [x/mat_in[row_idx][col_idx] for x in mat_in[row_idx]]
+    return mat_in
+
+
 def row_operation(mat_in, rows, scalar, operation):
     """
     This function performs an operation involving one or two rows of a matrix.
@@ -36,20 +50,17 @@ def row_operation(mat_in, rows, scalar, operation):
     new_mat = deepcopy(mat_in)
 
     # alter a row by a scalar if only one row index is given
-    if len(rows) == 1 and operation == '/':
+    if len(rows) == 1 and operation == '*':
         new_mat[rows[0]] = [idx / scalar for idx in mat_in[rows[0]]]
-        return new_mat
-    elif len(rows) == 1 and operation == '*':
-        new_mat[rows[0]] = [idx * scalar for idx in mat_in[rows[0]]]
-        return new_mat
 
     # alter a row by a scalar and add/subtract it to/from another row if two row indexes are given
     if len(rows) == 2 and operation == "+":
         new_mat[rows[1]] = list(map(lambda x, y: scalar*x + y, mat_in[rows[1]], mat_in[rows[0]]))
-        return new_mat
     elif len(rows) == 2 and operation == "-":
         new_mat[rows[1]] = list(map(lambda x, y: scalar*x - y, mat_in[rows[1]], mat_in[rows[0]]))
-        return new_mat
+
+    # return the altered matrix
+    return new_mat
 
 
 def mat_inv(in_mat):
@@ -60,5 +71,19 @@ def mat_inv(in_mat):
     :return: list of lists, i.e. a matrix
     """
 
-    # implement me
+    # define miscellaneous
+    nr_rows = len(in_mat)
+    nr_cols = len(in_mat[0])
+
+    # initialize the matrix inverse as an identity matrix with dimension of the original matrix
+    # used to build the augmented matrix
+    my_inv = [[1 if idx==jdx else 0 for idx in range(nr_rows)] for jdx in range(nr_cols)]
     pass
+
+
+# my_mat = [[3, 1, 2], [2, 0, -2], [0, 1, 1]]
+# nr_rows = len(my_mat)
+# nr_cols = len(my_mat[0])
+#
+# abc = pivot(my_mat, 0, 0)
+# print(abc)
