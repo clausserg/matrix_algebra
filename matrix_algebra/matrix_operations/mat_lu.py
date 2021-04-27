1241e02c424fe2f46587bfb3a94a575a607bc953
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-04-27 17:52:21 -0400

    improved description in mat_inverse and mat_lu

--- matrix_algebra/matrix_operations/mat_lu.py
+++ matrix_algebra/matrix_operations/mat_lu.py
@@ -1,3 +1,12 @@
+"""
+Module:
+mat_lu.py
+
+This module implements functions needed  to perform a LU factorization
+of a square matrix using Gaussian elmination.
+"""
+
+
 from functools import reduce
 
 
@@ -6,24 +15,33 @@ def swap_rows(mat_in, row,  col):
     This function swaps a matrix row with another row.
     Swaping will be done if the element at the position specified by the user
     via the 'row' and 'col' variables, is zero. Swaping occurs with a row that
-    has a non-zero element in the 'row','col' position. If all rows contain a
+    has a non-zero element in the same 'col'. If all rows contain a
     zero at the 'row', 'col' position, the original input matrix is returned.
 
     :param mat_in: list of lists, i.e. a matrix
-    :param row: int
-    :param col: int
+    :param row: int, row index in a matrix
+    :param col: int, column index in a matrix
     :return: tuple, a list of lists and a multiplier that is either 1 if no
     swapping has been done, or -1 if swapping was done.
     """
+
     if mat_in[row][col] != 0:
+        # return the original matrix if the matrix element at the
+        # input [row][col] position is not zero
         multiplier = 1
         return mat_in, multiplier
     else:
+        # find a row with non-zero matrix element in the same [col] position
+        # and swap. If swaping is done, the matrix determinant should be multiplied
+        # by -1, therefore a multiplier = -1 is returned togeteher with the matrix with
+        # swapped rows.
         for idx in range(row+1, len(mat_in)):
             if mat_in[idx][col] != 0:
                 mat_in[row], mat_in[idx] = mat_in[idx], mat_in[row]
                 multiplier = -1
                 return mat_in, multiplier
+        # if the if-else block is skipped, means all rows have a zero
+        # matrix element along the [col], in this case we return the original matrix
         multiplier = 1
         return mat_in, multiplier
 
@@ -45,7 +63,8 @@ def mat_lu(in_mat):
     my_det = 1
 
     # check if the input matrix contains a 0 on the first row first column.
-    # if true, swap the row with another row that does not contain a 0 element.
+    # if true, swap the row with another row that does not contain a 0 element
+    # and alter the determinant by a (-1) factor.
     in_mat, swapped = swap_rows(in_mat, 0, 0)
     my_det *= swapped
 
@@ -65,20 +84,21 @@ def mat_lu(in_mat):
 
     # zero beyond the first column
     for idx in range(len(upper_mat)):
-        # initialize a count for the line of reference for the elimination
-        # it will always be equal to the column index, jdx, and we contain it where
+        # initialize a count for the row of reference for the elimination
+        # it will always be equal to the column index, jdx, and we contain it here
         # in a 'ref_line_count' variable to keep it constant per "idx" row iteration
         ref_line_count = -1
-        for jdx in range(len(upper_mat[0])):
-            # increment reference row count: for col 1 will be ref line 1, for column 2
-            # it will be ref line 2, for column 3 it will be refline 3 etc.
+        for jdx in range(len(upper_mat[0])):  # iterate through columns
+            # increment reference row count: for col 1 will be ref row 1, etc.
             ref_line_count += 1
-            # we only need to start from second row and go between column 1 and below the main diagonal
+            # we only need to start from second row and go between column 1 and
+            # below the main diagonal
             if idx >= 2 and 1 <= jdx < idx and upper_mat[idx][jdx] != 0:
-                # check if reference element is zero and swap rows if true
+                # check if reference element is zero, swap rows if true
+                # and factor a (-1) in the matrix determinant
                 upper_mat, swapped = swap_rows(upper_mat, ref_line_count, jdx)
                 my_det *= swapped
-                # calculate the factor w.r.t. the reference line counter (will always be equal to jdx=column index)
+                # calculate the factor w.r.t. the reference row counter (will always be equal to jdx=column index)
                 factor = (-1) * upper_mat[idx][jdx] * (1 / upper_mat[ref_line_count][jdx])
                 upper_mat[idx][jdx] = upper_mat[idx][jdx] + factor * upper_mat[ref_line_count][jdx]
                 # add the factor to the L matrix
