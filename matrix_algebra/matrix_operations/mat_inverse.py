8762b6484a3aebb719afe7ff01ff68cc160cf250
Dumitru-Claudiu Sergentu <claudiu.sergentu@gmail.com>
2021-04-26 22:38:41 -0400

    implemented matrix inverse, exceptions await dealing with

--- matrix_algebra/matrix_operations/mat_inverse.py
+++ matrix_algebra/matrix_operations/mat_inverse.py
@@ -6,37 +6,23 @@ This module finds the inverse of a matrix by the Gauss-Jordan method
 """
 
 from copy import deepcopy
+from mat_lu import swap_rows
 
 
-def swap_rows(mat_in, row_a,  row_b):
+def pivot(mat_in, idx):
     """
-    This function swaps a matrix row with another row.
+    This function creates pivot in a idx diagonal position of mat_in
 
     :param mat_in: list of lists, i.e. a matrix
-    :param row: int
-    :param col: int
+    :param idx: int
     :return: list of lists, i.e. a matrix
     """
 
-    mat_in[row_a], mat_in[row_b] = mat_in[row_b], mat_in[row_a]
+    mat_in[idx] = [x/mat_in[idx][idx] for x in mat_in[idx]]
     return mat_in
 
 
-def pivot(mat_in, row_idx,  col_idx):
-    """
-    This function creates pivot in a given row/col position of the input matrix
-
-    :param mat_in: list of lists, i.e. a matrix
-    :param row_idx: int
-    :param col_idx: int
-    :return: list of lists, i.e. a matrix
-    """
-
-    mat_in[row_idx] = [x/mat_in[row_idx][col_idx] for x in mat_in[row_idx]]
-    return mat_in
-
-
-def row_operation(mat_in, rows, scalar, operation):
+def row_operation(mat_in, rows, scalar):
     """
     This function performs an operation involving one or two rows of a matrix.
 
@@ -49,15 +35,12 @@ def row_operation(mat_in, rows, scalar, operation):
     # work with a deepcopy of the input matrix
     new_mat = deepcopy(mat_in)
 
-    # alter a row by a scalar if only one row index is given
-    if len(rows) == 1 and operation == '*':
-        new_mat[rows[0]] = [idx / scalar for idx in mat_in[rows[0]]]
-
-    # alter a row by a scalar and add/subtract it to/from another row if two row indexes are given
-    if len(rows) == 2 and operation == "+":
-        new_mat[rows[1]] = list(map(lambda x, y: scalar*x + y, mat_in[rows[1]], mat_in[rows[0]]))
-    elif len(rows) == 2 and operation == "-":
-        new_mat[rows[1]] = list(map(lambda x, y: scalar*x - y, mat_in[rows[1]], mat_in[rows[0]]))
+    if len(rows) == 1:
+        # alter a row by a scalar if only one row index is given
+        new_mat[rows[0]] = [idx * scalar for idx in mat_in[rows[0]]]
+    else:
+        # alter a row by a scalar and add/subtract it to/from another row
+        new_mat[rows[1]] = list(map(lambda x, y: scalar*x + y, mat_in[rows[0]], mat_in[rows[1]]))
 
     # return the altered matrix
     return new_mat
@@ -75,15 +58,32 @@ def mat_inv(in_mat):
     nr_rows = len(in_mat)
     nr_cols = len(in_mat[0])
 
-    # initialize the matrix inverse as an identity matrix with dimension of the original matrix
+    # initialize the matrix inverse as an identity matrix
     # used to build the augmented matrix
     my_inv = [[1 if idx==jdx else 0 for idx in range(nr_rows)] for jdx in range(nr_cols)]
-    pass
+
+    # transform the input matrix in the augmented matrix to work with
+    [in_mat[row].extend(my_inv[row]) for row in range(nr_rows)]
+
+    for col in range(nr_cols):
+        # swap rows if zero prevents making the pivot
+        in_mat = swap_rows(in_mat, col, col)[0]
+        # make pivot
+        in_mat = pivot(in_mat, col)
+        # eliminate the column
+        for row in range(nr_rows):
+            if row == col:
+                continue  # skip the row with the pivot
+            else:
+                in_mat = row_operation(in_mat, [col, row], (-1) * in_mat[row][col])
+
+    # return the final augmented matrix
+    return in_mat
 
 
-# my_mat = [[3, 1, 2], [2, 0, -2], [0, 1, 1]]
-# nr_rows = len(my_mat)
-# nr_cols = len(my_mat[0])
-#
-# abc = pivot(my_mat, 0, 0)
-# print(abc)
+# TESTING THE CODE
+my_mat = [[5, 2, 3, 5], [1, 9, 2, 6], [0, 2, 2, 2], [0, 2, 3, 4]]
+
+
+for line in mat_inv(my_mat):
+    print(line)
